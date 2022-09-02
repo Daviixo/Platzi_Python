891af4fe9c698a91e87cb0cd3591f5c361177f7f
David Diaz <daviidvx93@gmail.com>
2022-09-02 10:24:02 -0600

    Updates cisco command test

---
+++ cisco_test.py
@@ -0,0 +1,28 @@
+from netmiko import ConnectHandler
+
+def main():
+
+    get_host_name = input('Host name? (Do NOT add expertcity.com! Example: asr1k.las\n')
+    get_username = 'ddiaz'
+    get_password = input('Please input your password.\n')
+    run_command(get_host_name, get_username, get_password)
+
+def run_command(s_host, s_username, s_password):
+
+    final_host = str(s_host) + '.expertcity.com'
+
+    net_connect = ConnectHandler(
+        device_type = 'cisco_ios',
+        host = final_host,
+        username = s_username,
+        password = s_password,
+    )
+
+    output = net_connect.send_command(
+        "show ip bgp all neighbors 10.16.10.52 | include Description|BGP state|Interface associated|Last reset"
+    )
+    print(output)
+
+
+if __name__ == '__main__':
+    main()
